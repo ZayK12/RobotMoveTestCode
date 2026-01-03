83b9e4ad9e954795709a584293721f943321ac26
ZayK12 <27kazi-zayyaan@jonesboroschools.net>
2026-01-03 12:30:00 -0600

    Odometry.h update
    Looks like i forgot to include odometry.h in the last 2 commits so heres the update

--- ROBOTCODE/odometry.h
+++ ROBOTCODE/odometry.h
@@ -32,7 +32,7 @@ public:
     /// @param distanceBack  distance from back encoder of the robot to tracking center
     /// @param _rightEncoder pointer to right encoder
     /// @param backEncoder pointer to back encoder
-    odometry(std::vector<double> initPos, double initOrientation, double wheelC, double distanceLeft, double distanceRight, double distanceBack, pros::Rotation* _rightEncoder, pros::Rotation* backEncoder, pros::v5::IMU* _leftIMU, pros::v5::IMU* _rightIMU);
+    odometry(std::vector<float> initPos, float initOrientation, float wheelC, float distanceLeft, float distanceRight, float distanceBack, pros::Rotation* _rightEncoder, pros::Rotation* backEncoder, pros::v5::IMU* _leftIMU, pros::v5::IMU* _rightIMU);
 
 
     /**
@@ -43,7 +43,7 @@ public:
      * @param distanceLeft distance from left encoder base to tracking center
      * @param distanceRight distance from right encoder base to tracking center
      * @param distanceBack  distance from back encoder of the robot to tracking center */
-    odometry(double wheelC, double distanceLeft, double distanceRight, double distanceBack,pros::Rotation* _rightEncoder, pros::Rotation* backEncoder, pros::v5::IMU* _leftIMU, pros::v5::IMU* _rightIMU);
+    odometry(float wheelC, float distanceLeft, float distanceRight, float distanceBack,pros::Rotation* _rightEncoder, pros::Rotation* backEncoder, pros::v5::IMU* _leftIMU, pros::v5::IMU* _rightIMU);
     
    
     /**
@@ -52,14 +52,14 @@ public:
      * @brief Updates position values w/ changing ram values
      * @param newX new X value
      * @param newY new Y value */
-    void updatePosition(double newX, double newY);
+    void updatePosition(float newX, float newY);
 
     /**
      * @author Zayyaan K
      * @date 5/9/25
      * @brief Updates position values w/ changing ram values using a vector
      * @param newPos new position. */
-    void updatePosition(std::vector<double> newPos);
+    void updatePosition(std::vector<float> newPos);
 
     /**
 	* @author Zayyaan K
@@ -67,78 +67,74 @@ public:
 	* @brief Updates Orientation and position values w/ changing ram values using a vector
 	* @param newPos new position vector
     * @param newOrientation new orientation value*/
-    void updatePosition(std::vector<double> newPos, double newOrientation);
+    void updatePosition(std::vector<float> newPos, float newOrientation);
     
     /**
     * @author Zayyaan K
 	* @date 5/9/25
     * @brief Gets the pointer to the position vector
 	* @return pointer to the position vector */
-    std::vector<double>* getPositionPointer();
+    std::vector<float>* getPositionPointer();
 
 	/**
 	 * @author Zayyaan K
 	 * @date 6/4/25
 	 * @brief Gets the pointer to the orientation value
 	 * @return pointer to the orientation value (radians)*/
-    double* getOrientationPointer();
+    float* getOrientationPointer();
     
 
-    std::vector<double> MainPosition;
+    std::vector<float> MainPosition;
     /**
      * @brief updates encoder values
      * @note also automatically calls the calculate local offset, may need to do some c++ trickery */
     void updateDistances();
 
-    /**
-     * @note this is the old version of the update distances retained in case of the patch not working properly
-     * @author Zayyaan K
-     * @date 8/20/25
-     * @brief old version of update distances
-     */
-    void updateDistancesOld();
 private:
     
-    std::vector<double>* directPositionPtr;
-    double orientation; 
-    const double disL;
-    const double disR;
-    const double disB;
-    const double wheelCircum;
+    std::vector<float>* directPositionPtr;
+    float orientation; 
+    const float disL;
+    const float disR;
+    const float disB;
+    const float wheelCircum;
     pros::Rotation* rightEncoder;
     pros::Rotation* backEncoder;
     pros::v5::IMU* leftIMU;
     pros::v5::IMU* rightIMU;
-    const double TOLERANCE = 1e-3; //Tolerance for floating point comparisons
+    const float TOLERANCE = 1e-5; //Tolerance for floating point comparisons
     
-    std::vector<double> localOffset;
+    std::vector<float> localOffset;
     /**
      * @brief  This function is to prevent the IMU to return a rotation of 360 as 0 is preferred
      * @param deg the variable to check*/
-    double overflowCheck(double deg);
+    float overflowCheck(float deg);
     /**
      * @brief gets the delta (absolute difference) of 2 radians
      * @param rad1 Radian 1
      * @param rad2 Radian 2
      * @return the change between rad1 and rad2. */
-    double subRadians(double rad1, double rad2);
+    float subRadians(float rad1, float rad2);
     /**
-     * @brief Converts a coordinate set from cartesian degrees to polar
+     * @brief gets the delta (absolute difference) of 2 degrees, normalized to [-180, 180]
+     * @param deg1 Degree 1
+     * @param deg2 Degree 2
+     * @return the change between deg1 and deg2. */
+    float subDegrees(float deg1, float deg2);
+    /**
      * @param coordSet pointer to vector holding doubles. Typically {0,0}. Mutates them */
-    void cartesianToPolar(std::vector<double>& coordSet);
+    void cartesianToPolar(std::vector<float>& coordSet);
     /**
      * @brief converts a polar set of coordinates to a cartesian set of coordinates
      * @param radius radius from polar set
      * @param theta theta from polar set*/
-    void polarToCartesian(double& radius, double& theta);
+    void polarToCartesian(float& radius, float& theta);
     
-    
-
-
 
     /**
      * @brief updates the changes in X and Y values*/
-    void calculateLocalOffset(double backInchDelta_, double rightInchDelta_, double headingDelta_);
+    void calculateLocalOffset(float backInchDelta_, float rightInchDelta_, float headingDelta_);
+
     /**
      * @brief Fixes the rotation offset and updates the global position values */
     void rotateToGlobalFrame();
