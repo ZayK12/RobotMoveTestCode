0e01363f567a03a4838e47f6205f58b793a1482c
ZayK12 <27kazi-zayyaan@jonesboroschools.net>
2026-01-16 13:00:00 -0600

    PID.cpp fix
    date this as jan 16th
    
    Finished the code and made it actually work.
    
    Removed strafe motor code as it was unused
    
    moved to floats to reduce computational time
    
    Now calculates turningError
    
    Path completion changed to a faster version which checks to see if the final point in path flag is true, then stops if the robot is within the error margin. Rather than the old system that used a bunch of other flags but may still be implemented later
    
    updated the debug system to use printf
    
    use float specific math functions (cosf & sinf not cos & sin)
    
    Changes in PID.h reflect the changes made in PID.cpp

--- ROBOTCODE/PID.cpp
+++ ROBOTCODE/PID.cpp
@@ -1,18 +1,32 @@
 #include "PID.h"
-
-PID::PID(std::vector<double>* Point, std::vector<double>* position, double* heading, double& _maxDSpeed, double& _maxTSpeed, double& _maxStrafeSpeed, double& _DKp, double& _DKi, double& _DKd, double& _TKp, double& _TKi, double& _TKd,double& _SKp, double& _SKi, double& _SKd, pros::MotorGroup* _leftMotors, pros::MotorGroup* _rightMotors, pros::Motor* _strafeMotor) :
-	targetGlobal(Point), currentPosition(position), currentHeading(heading), maxDSpeed(_maxDSpeed), maxTurnSpeed(_maxTSpeed), maxStrafeSpeed(_maxStrafeSpeed), DKp(_DKp), DKi(_DKi), DKd(_DKd), TKp(_TKp), TKi(_TKi), TKd(_TKd), SKp(_SKp), SKi(_SKi), SKd(_SKd),
+#include "config.h"
+PID::PID(std::vector<float>* Point, std::vector<float>* position, float* heading, float& _maxDSpeed, float& _maxTSpeed, float& _DKp, float& _DKi, float& _DKd, float& _TKp, float& _TKi, float& _TKd, pros::MotorGroup* _leftMotors, pros::MotorGroup* _rightMotors) :
+	targetGlobal(Point), currentPosition(position), currentHeading(heading), maxDSpeed(_maxDSpeed), maxTurnSpeed(_maxTSpeed), DKp(_DKp), DKi(_DKi), DKd(_DKd), TKp(_TKp), TKi(_TKi), TKd(_TKd),
 	errorDistance(0.0), errorTurning(0.0), integral(0.0), derivative(0.0), previousError(0.0), turnIntegral(0.0), turnDerivative(0.0), turnPreviousError(0.0),
-	errorSumMargin(1.0), turnErrorMargin(1.0), leftside(_leftMotors), rightside(_rightMotors), strafeMotor(_strafeMotor) {}
+	errorSumMargin(1.0), turnErrorMargin(1.0), leftside(_leftMotors), rightside(_rightMotors) {}
 
-PID::PID(std::vector<double>* Point, std::vector<double>* position, double* heading, double& _maxDSpeed, double& _maxTSpeed, double& _maxStrafeSpeed, double& _DKp, double& _DKi, double& _DKd, double& _TKp, double& _TKi, double& _TKd, double& _SKp, double& _SKi, double& _SKd, double& ERM, double& TERM, double& SERM, double& ESM, int& pathDiv, double& MPCA, pros::MotorGroup* _leftMotors, pros::MotorGroup* _rightMotors, pros::Motor* _strafeMotor) :
-	targetGlobal(Point), currentPosition(position), currentHeading(heading), maxDSpeed(_maxDSpeed), maxTurnSpeed(_maxTSpeed), maxStrafeSpeed(_maxStrafeSpeed),DKp(_DKp), DKi(_DKi), DKd(_DKd), TKp(_TKp), TKi(_TKi), TKd(_TKd), SKp(_SKp), SKi(_SKi), SKd(_SKd),
+PID::PID(std::vector<float>* Point, std::vector<float>* position, float* heading, float& _maxDSpeed, float& _maxTSpeed, float& _DKp, float& _DKi, float& _DKd, float& _TKp, float& _TKi, float& _TKd, float& ERM, float& TERM, float& ESM, int& pathDiv, float& MPCA, pros::MotorGroup* _leftMotors, pros::MotorGroup* _rightMotors) :
+	targetGlobal(Point), currentPosition(position), currentHeading(heading), maxDSpeed(_maxDSpeed), maxTurnSpeed(_maxTSpeed),DKp(_DKp), DKi(_DKi), DKd(_DKd), TKp(_TKp), TKi(_TKi), TKd(_TKd),
 	errorDistance(0.0), errorTurning(0.0), integral(0.0), derivative(0.0), previousError(0.0), turnIntegral(0.0), turnDerivative(0.0), turnPreviousError(0.0), 
-	errorSumMargin(ERM), turnErrorMargin(TERM), strafeErrorMargin(SERM), errorMargin(ESM), pathDivamt(pathDiv), motorPowerCheckAmt(MPCA), leftside(_leftMotors), rightside(_rightMotors), strafeMotor(_strafeMotor) {}
-bool PID::errorSumUpdate(double inputNumber) {
-	static std::vector<double> numberList(10, 0.0); // List to store the last 10 numbers
+	errorSumMargin(ERM), turnErrorMargin(TERM), errorMargin(ESM), pathDivamt(pathDiv), motorPowerCheckAmt(MPCA), leftside(_leftMotors), rightside(_rightMotors) {}
+
+void PID::setLateralConstants(float newDKp, float newDKi, float newDKd){
+	DKp = newDKp;
+	DKi = newDKi;
+	DKd = newDKd;
+}
+void PID::setTurningConstants(float newTKp, float newTKi, float newTKd){
+	TKp = newTKp;
+	TKi = newTKi;
+	TKd = newTKd;
+}
+
+
+
+bool PID::errorSumUpdate(float inputNumber) {
+	static std::vector<float> numberList(10, 99999); // List to store the last 10 numbers 99999 is used to prevent early triggering
 	//errorSum variable stores the sum of the last 10 numbers
-	static double errorSum = 0;
+	static float errorSum = 0;
 	// index for the circular buffer 
 	static int index = 0;
 	// updates the errorSum variable by subtracting the soon-to-be-removed number
@@ -21,105 +35,119 @@ bool PID::errorSumUpdate(double inputNumber) {
 	numberList[index] = inputNumber;
 	// Adds new number to the sum
 	errorSum += inputNumber;
+	printf("errorSum: %.3f\n", errorSum);
 	// Increments the index and uses modulus to wrap around. Ex: 11 % 10 = 1
 	index = (index + 1) % 10; // Wraps around to 0 after reaching 9
-	if (fabs((errorSum / 10) - inputNumber) < errorSumMargin) {
+	if (fabsf((errorSum / 10) - inputNumber) < errorSumMargin) {
+		//DKp = 0;
+		//TKp = 0;
 		return true; // Returns true if the average of the last 10 numbers is less than the error margin
 	}
 	else {
 		return false; // Returns false if the average of the last 10 numbers is greater than the error margin
 	}
 }
-std::vector<double> PID::cartesianToClock(const std::vector<double>& localVector) {
-	std::vector<double> clockTarget(2);
+std::vector<float> PID::cartesianToClock(const std::vector<float>& localVector) { // DEPRECIATED, REPLACED WITH INLINE FUNCTION
+	std::vector<float> clockTarget(2);
 	clockTarget[0] = sqrt((localVector[0] * localVector[0]) + (localVector[1] * localVector[1]));
 	clockTarget[1] = atan2(localVector[1], localVector[0]);
 	return clockTarget;
 }
-double PID::deltaDegrees(double a, double b) {
-	double delta = fmod(b - a + 180.0, 360.0) - 180.0;
+float PID::deltaDegrees(float a, float b) {
+	float delta = fmodf(b - a + 180.0, 360.0);
+	if (delta < 0) delta += 360.0;
+	delta -= 180.0;
 	return delta;
 }
 
 
 void PID::pidUpdate(){
 	// Calculate local target vector (relative to robot)
-	std::vector<double> targetLocal = {
-		(*targetGlobal)[0] - (*currentPosition)[0],
-		(*targetGlobal)[1] - (*currentPosition)[1]
-	};
-	//double targetHeading = (*targetGlobal)[3];
-	double targetHeading = 0; //Placeholder until the vector above gets patched
-
-	double theta = (*currentHeading) * M_PI / 180;
-	double localizedX = targetLocal[0] * cos(theta) - targetLocal[1] * sin(theta);
-	double localizedY = -targetLocal[0] * sin(theta) + targetLocal[1] * cos(theta);
+	float targetLocalX = (*targetGlobal)[0] - (*currentPosition)[0];
+	float targetLocalY = (*targetGlobal)[1] - (*currentPosition)[1];
+	float theta = (*currentHeading) * (M_PI/180.0f); // heading in radians
+	
+	// Rotate the global target vector into the robot-local frame (rotate by -theta)
+	// Standard frame: X = forward (drive), Y = right (strafe)
+	float localizedX = targetLocalX * cosf(theta) + targetLocalY * sinf(theta);   
+	float localizedY = -targetLocalX * sinf(theta) + targetLocalY * cosf(theta);  
 
 
 
 
-	/*
-	// Takes the local target and converts it to clock coordinates
-	std::vector<double> clockTarget = cartesianToClock(targetLocal);
-	errorDistance = clockTarget[0];
-	errorTurning = deltaDegrees(*currentHeading, clockTarget[1]);
-	*/
+	// errorDistance is forward motion (X component in robot frame)
+	errorDistance = localizedX;
+
+	float errorTurning = atan2(localizedY, localizedX) * 180.0 / M_PI; // angle in degrees
+
+	if(config::debugPID){
+		//printf("\nLX:%.3f, LY:%.3f, ED:%.3f, ET:%.3f", localizedX, localizedY, errorDistance, errorTurning);
+		//printf("\nAngle to Target: %.3f", angleToTarget);
+	}
+	
+
 
 	// If target is behind, reverse direction
-	/* This function prbobaly works, I just don't like it, so I will comment it out for now.
-	if (fabs(errorTurning) > 90) {
+	if (fabsf(errorTurning) > 90 && (*targetGlobal)[4] == 1) {
 		errorDistance = -errorDistance;
-		errorTurning = deltaDegrees(180, errorTurning);
+		errorTurning = deltaDegrees(errorTurning, 180);
+		if(config::debugPID){
+			printf(" Reversing\n\n\n\n\n\n\n\n");
+		}
 	}
-	*/
-	// If robot is close enough to end point, stop
-	if (fabs(errorDistance) < errorMargin) {
-		errorDistance = 0;
-		integral = 0;
-	}
+
+	
 	//Updates ID variables
 	integral += errorDistance;
-	double deriviative = errorDistance - previousError;
+	float deriviative = errorDistance - previousError;
 	previousError = errorDistance;
 	//Updates the turning ID variables
 	turnIntegral += errorTurning;
-	double turnDerivative = errorTurning - turnPreviousError;
+	float turnDerivative = errorTurning - turnPreviousError;
 	turnPreviousError = errorTurning;
 
 	// Calculates the power amounts for distance and turning
-	double maxSpeed = (*targetGlobal)[2];
-	
-	//double powerDistance = DKp * errorDistance + DKi * integral + DKd * derivative; // Pid formula
-	double powerDistance = DKp * localizedX + DKi * integral + DKd * derivative; // Pid formula
-	if (powerDistance > maxSpeed) { // Speed cap
-		powerDistance = maxSpeed;
-	}
-	else if (powerDistance < -maxSpeed) {
-		powerDistance = -maxSpeed;
-	}
-	double powerTurning = TKp * errorTurning + TKi * turnIntegral + TKd * turnDerivative; // Pid formula
-	if (powerTurning > maxTurnSpeed) { // Speed cap
-		powerTurning = maxTurnSpeed;
-	}
-	else if (powerTurning < -maxTurnSpeed) {
-		powerTurning = -maxTurnSpeed;
-	}
-	double powerStrafe = SKp * errorStrafe * SKi * strafeIntegral + SKd * strafeDerivative;
+	float maxSpeed = (*targetGlobal)[2];
+	float maxTurnSpeed = (*targetGlobal)[3];
+
+	float powerDistance = DKp * errorDistance + DKi * integral + DKd * derivative; // Pid formula
+	//float powerDistance = DKp * localizedX + DKi * integral + DKd * derivative; // Pid formula
+	powerDistance = std::clamp(powerDistance, -maxSpeed, maxSpeed); // Keep values between max speed in both directions
+
+	float powerTurning = TKp * errorTurning + TKi * turnIntegral + TKd * turnDerivative; // Pid formula
+	powerTurning = std::clamp(powerTurning, -maxTurnSpeed, maxTurnSpeed);
+
+	float powerStrafe = SKp * errorStrafe * SKi * strafeIntegral + SKd * strafeDerivative;
 	if (powerStrafe > maxStrafeSpeed){
 		powerStrafe = maxStrafeSpeed;
 	}
 	else if (powerStrafe < -maxStrafeSpeed){
 		powerStrafe = -maxStrafeSpeed;
 	}
-	double distanceFromPoint = sqrt(pow(targetLocal[0], 2) + pow(targetLocal[1], 2));
-	if (errorSumUpdate(distanceFromPoint)) {
-		std::cout << "Below Margin" << std::endl;
+	if (fabsf(errorDistance) < errorMargin && finalPointInPath) {
+		powerDistance = 0;
+		powerTurning = 0;
+		integral = 0;
+		turnIntegral = 0;
+		finalPointInPath = false;
+		pathComplete = true;
 	}
-	else {
-		leftside->move_voltage(powerDistance + powerTurning);
-		rightside->move_voltage(powerDistance - powerTurning);
-		strafeMotor->move_voltage(powerStrafe);
-		std::cout << "Power Distance left: " << powerDistance + powerTurning << "Power Distance Right:" << powerDistance - powerTurning << std::endl;
+	if(errorDistance < turnErrorMargin && finalPointInPath){ // If robot is close enough to the final point, turn off turning controller to prevent osicilations
+		powerTurning = 0;
+	}
+	/*
+	//float distanceFromPoint = sqrt(pow(targetLocal[0], 2) + pow(targetLocal[1], 2));
+	if (!errorSumUpdate(errorDistance) && config::debugPID){
+		printf("Point reached!");
+	}
+	*/
+		
+	leftside->move(powerDistance - powerTurning);
+	rightside->move(powerDistance + powerTurning);
+	if(config::debugPID){
+		printf("\errorTurning: %.3f,", errorTurning);
+		//printf("\nPDL:%.3f, PDR:%.3f, PT:%.3f,", powerDistance+powerTurning, powerDistance - powerTurning, powerTurning);
+		//std::cout <<"Power Distance left: " << powerDistance + powerTurning << "Power Distance Right:" << powerDistance - powerTurning  << "\nPower Turning: " << powerTurning << std::endl;
 	}
 
 
--- ROBOTCODE/PID.h
+++ ROBOTCODE/PID.h
@@ -4,8 +4,11 @@
 #include <vector>
 #include <list>
 #include <array>
+#include "main.h"
 #include "api.h"
 #include "pros/adi.hpp"
+#include "pros/motors.hpp"      // For pros::Motor
+#include "pros/motor_group.hpp" // For pros::MotorGroup
 class PID
 {
 	public:
@@ -23,8 +26,8 @@ class PID
 		* @param   _DKd Forward Derivative constant.			 (_ is to prevent name clashes)
 		* @param   _TKp Turning Proportional constant.			 (_ is to prevent name clashes)
 		* @param   _TKi Turning Integral constant.				 (_ is to prevent name clashes)
-		* @param   _TKd Turning Derivative constant.*/	      // (_ is to prevent name clashes)
-		PID(std::vector<double>* Point, std::vector<double>* position, double* heading, double _maxDSpeed, double _maxTSpeed, double _DKp, double _DKi, double _DKd, double _TKp, double _TKi, double _TKd, pros::MotorGroup* _leftMotors, pros::MotorGroup* _rightMotors);
+		* @param   _TKd Turning Derivative constant.	    */// (_ is to prevent name clashes)
+		PID(std::vector<float>* Point, std::vector<float>* position, float* heading, float& _maxDSpeed, float& _maxTSpeed,float& _DKp, float& _DKi, float& _DKd, float& _TKp, float& _TKi, float& _TKd, pros::MotorGroup* _leftMotors, pros::MotorGroup* _rightMotors);
 		/**
 		* @author  Zayyaan K
 		* @date    5/21/25
@@ -34,42 +37,84 @@ class PID
 		* @param   ESM error sum margin (read function to understand)
 		* @param   pathDiv number of divisions for the final path
 		* @param   MPCA motor power check amount (if the motor power is less than this, stop the robot)
+		* All other parameters are the same as the other constructor.
 		*/
-		PID(std::vector<double>* Point, std::vector<double>* position, double* heading, double _maxDSpeed, double _maxTSpeed, double _DKp, double _DKi, double _DKd, double _TKp, double _TKi, double _TKd, double ERM, double TERM, double ESM, int pathDiv, double MPCA, pros::MotorGroup* _leftMotors, pros::MotorGroup* _rightMotors);
+		PID(std::vector<float>* Point, std::vector<float>* position, float* heading, float& _maxDSpeed, float& _maxTSpeed, float& _DKp, float& _DKi, float& _DKd, float& _TKp, float& _TKi, float& _TKd,float& ERM, float& TERM, float& ESM, int& pathDiv, float& MPCA, pros::MotorGroup* _leftMotors, pros::MotorGroup* _rightMotors);
 		/**
 		* @author  Zayyaan K
 		* @date    5/21/25
 		* @brief   updates pid values
 		*/
 		void pidUpdate();
+
+		/**
+		 * @author  Zayyaan K
+		 * @date    9/2/25
+		 * @brief   Sets global target point
+		 */
+		void setTargetPoint();
 		
+		/**
+		 * @author  Zayyaan K
+		 * @date    9/2/25
+		 * @brief   changes the max D speed
+		 * @param   newMaxSpeed new max speed*/
+		void setMaxDSpeed(float newMaxSpeed);
+		/**
+		 * @author  Zayyaan K
+		 * @date    9/2/25
+		 * @brief   changes the max turn speed
+		 * @param   newMaxTurnSpeed new max turn speed */
+		void setMaxTurnSpeed(float newMaxTurnSpeed);
+		/**
+		 * @author Zayyaan K
+		 * @date 12/26/25
+		 * @brief Sets lateral constants
+		 * @param newDKp new proprotional constant for lateral movement
+		 * @param newDKi new integral constant for lateral movement
+		 * @param newDKd new derivative constant for lateral movement */
+		void setLateralConstants(float newDKp, float newDKi, float newDKd);
+		/**
+		 * @author Zayyaan K
+		 * @date 12/26/25
+		 * @brief Sets turning constants
+		 * @param newTKp new proprotional constant for turning
+		 * @param newTKi new integral constant for turning
+		 * @param newTKd new derivative constant for turning */
+		void setTurningConstants(float newTKp, float newTKi, float newTKd);
+
+		bool finalPointInPath = false;
+		bool pathComplete = false;
 	private:
-		std::vector<double>* targetGlobal; // Pointer to the target point	
-		std::vector<double>* currentPosition; //Pointer to the robot's position
-		std::vector<double>* targetLocal; // Pointer to the target point
+		std::vector<float>* targetGlobal; // Pointer to the target point	
+		std::vector<float>* currentPosition; //Pointer to the robot's position
 		pros::MotorGroup* leftside; // Pointer to the left motors
   		pros::MotorGroup* rightside; // Pointer to the right motors
-		double* currentHeading; // Pointer to the robot's heading
-		double DKp, DKi, DKd; // Forward PID constants
-		double TKp, TKi, TKd; // Turning PID constants
-		double maxDSpeed, maxTurnSpeed = 80; // Maximum speed 
-		double errorDistance, errorTurning; // Error values
-		double integral, derivative, previousError; //ID variables
-		double turnIntegral, turnDerivative, turnPreviousError; //Turning ID variables
-		double errorSumMargin, turnErrorMargin, errorMargin = 1; // Error margin
+		pros::Motor* strafeMotor; // Pointer to the strafe motor (may be referred to as H motor)
+		float* currentHeading; // Pointer to the robot's heading (Radians)
+		float DKp, DKi, DKd; // Forward PID constants
+		float TKp, TKi, TKd; // Turning PID constants
+		float SKp, SKi, SKd; // Strafing PID constants
+		float maxDSpeed, maxTurnSpeed, maxStrafeSpeed; // Maximum speed 
+		float errorDistance, errorTurning, errorStrafe; // Error values
+		float integral, derivative, previousError; //ID variables
+		float turnIntegral, turnDerivative, turnPreviousError; //Turning ID variables
+		float strafeIntegral, strafeDerivative, strafePreviousError; // Strafing ID variables
+		float errorSumMargin, turnErrorMargin, errorMargin = 1; // Error margin
+		float strafeErrorMargin, strafeSumErrorMargin = 1; // Strafe error margins
 		int pathDivamt = 4; // Number of divisions for final path 
-		double motorPowerCheckAmt = 10.0; // if motor power is less than this, stop the robot
+		float motorPowerCheckAmt = 10.0; // if motor power is less than this, stop the robot
 
 		// 5/21/25 Modified version of my original errorSumUpdate function
 		// Uses a circular buffer rather than the old linear one
 		//goal of function is to delete the oldest number (last number) and add the new number upfront and update //the errorSum accordingly
-		bool errorSumUpdate(double inputNumber);
+		bool errorSumUpdate(float inputNumber);
 		
 		// Converts a vector from cartesian coordinates (x, y) to clock coordinates (radius, angle)
-		std::vector<double> cartesianToClock(const std::vector<double>& localVector);
+		std::vector<float> cartesianToClock(const std::vector<float>& localVector);
 
 		//Gets the angle between two points in degrees
-		double deltaDegrees(double a, double b); 
+		float deltaDegrees(float a, float b); 
 
 
 
